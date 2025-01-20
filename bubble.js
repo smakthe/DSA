4ae0ff8f73669c25b85b3dfabea8f16bc21789e5
Somak Chakraborty <scmakra99@gmail.com>
2025-01-20 09:31:01 +0000

    Small change

--- bubble.js
+++ bubble.js
@@ -4,9 +4,7 @@ console.log(a)
 for(i=0;i<a.length;i++){
     for(j=0;j<(a.length-i-1);j++){
         if(a[j]>a[j+1]){
-            temp = a[j];
-            a[j] = a[j+1];
-            a[j+1] = temp;
+            a[j+1] = [a[j], a[j] = a[j+1]][0]
         }
     }
 }
